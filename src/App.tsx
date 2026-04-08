480f4f6ec5ace82533c9afcc959af74a22c98bb4
Eduarda Vieira <eduarda.vieira.goncalves7@gmail.com>
2026-04-08 10:18:05 -0300

    feat: add example repository card with details and icons; update input styles for GitHub user search

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from './components/Header'
 import { InteressPill } from './components/InteressPill'
 import { ProfileSection } from './components/ProfileSection'
 import { TechPill } from './components/TechPill'
-import { MagnifyingGlass } from '@phosphor-icons/react'
+import { GitFork, MagnifyingGlass, Star } from '@phosphor-icons/react'
 
 export default function App() {
   return (
@@ -41,7 +41,7 @@ export default function App() {
           </section>
         </div>
 
-        <section className="mx-auto my-10">
+        <section className="mx-auto mt-10 mb-5">
           <div className="max-w-lg">
             <h2 className="text-center text-xl font-medium mb-3">Explorar</h2>
             <p className="text-secondary text-center">
@@ -55,7 +55,7 @@ export default function App() {
           <input
             type="text"
             placeholder="Digite um usuário do GitHub"
-            className="w-full rounded-md border border-card px-3 py-2 text-sm outline-none focus:border-primary"
+            className="w-full rounded-md border border-card px-3 py-2 text-sm outline-none focus:border-pink-400 focus:ring-1 focus:ring-pink-400 transition-all duration-200 ease-out"
             required
           />
           <button
@@ -66,6 +66,38 @@ export default function App() {
             Buscar
           </button>
         </form>
+
+        <section id="cards" className="grid md:grid-cols-3 gap-4 mt-10">
+          
+          <div className="border border-card rounded-md p-4">
+            <h3 className="font-medium text-md mb-3">Repositório Exemplo</h3>
+            <p className=" text-xs mb-3 line-clamp-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias tempore ipsa
+              impedit, eos voluptate doloribus sed. Alias, deleniti fugit repellat nostrum at
+              aspernatur blanditiis. Beatae sunt nemo facilis tempore laboriosam!
+            </p>
+            <div className="flex gap-1 mb-3">
+              <span className="text-[10px] leading-none bg-primary text-primary rounded-xl px-2 py-1 justify-center">
+                JavaScript
+              </span>
+              <span className="text-[10px] leading-none bg-primary text-primary rounded-xl px-2 py-1 justify-center">
+                React
+              </span>
+            </div>
+            <div className="text-secondary text-sm flex items-center gap-4 mt-2">
+              <div className="flex items-center gap-1">
+                <Star size={12} className="text-secondary" />
+                <span className="text-xs">1</span>
+              </div>
+
+              <div className="flex items-center gap-1">
+                <GitFork size={12} className="text-secondary" />
+                <span className="text-xs">0</span>
+              </div>
+            </div>
+          </div>
+
+        </section>
       </div>
       <Footer />
     </>
